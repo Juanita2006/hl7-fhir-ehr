e23e20d9e3a4ff01b134241d5cc26b7292c783f2
Juanita2006 <juanita.trujillo-n@mail.escuelaing.edu.co>
2025-04-04 00:03:45 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -12,7 +12,17 @@ app.add_middleware(
     allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
     allow_headers=["*"],  # Permitir todos los encabezados
 )
-
+@app.get("/patient", response_model=dict)
+async def get_patient_by_identifier(system: str, value: str):
+    print("solicitud datos:",system,value)
+    status,patient = GetPatientByIdentifier(system,value)
+    if status=='success':
+        return patient 
+    elif status=='notFound':
+        raise HTTPException(status_code=204, detail="Patient not found")
+    else:
+        raise HTTPException(status_code=500, detail=f"Internal error, (status)")
+        
 @app.get("/patient/{patient_id}", response_model=dict)
 async def get_patient_by_id(patient_id: str):
     status,patient = GetPatientById(patient_id)
@@ -23,18 +33,6 @@ async def get_patient_by_id(patient_id: str):
     else:
         raise HTTPException(status_code=500, detail=f"Internal error. {status}")
         
-@app.get("/patient/{patient_id}", response_model=dict)
-async def get_patient_by_identifier(system: str, value: str):
-    print("solicitud datos:",system,value)
-    status,patient = GetPatientByIdentifier(system,value)
-    if status=='success':
-        return patient 
-    elif status=='notFound':
-        raise HTTPException(status_code=204, detail="Patient not found")
-    else:
-        raise HTTPException(status_code=500, detail=f"Internal error, (status)")
-
-
 @app.post("/patient", response_model=dict)
 async def add_patient(request: Request):
     new_patient_dict = dict(await request.json())
@@ -43,7 +41,8 @@ async def add_patient(request: Request):
         return {"_id":patient_id}  # Return patient id
     else:
         raise HTTPException(status_code=500, detail=f"Validating error: {status}")
-
+from fastapi import HTTPException, Request 
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
