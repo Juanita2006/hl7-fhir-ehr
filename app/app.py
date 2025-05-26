db106b40b0d9490fc9f27c34dcff586b61ab0e63
Juanita2006 <juanita.trujillo-n@mail.escuelaing.edu.co>
2025-05-26 00:42:02 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
 import os
+import uvicorn
+from app.controlador.PatientCrud import GetPatientById,WritePatient,GetPatientByIdentifier
+from app.controlador.appointmentCrud import WriteAppointment
 
 # Importar funciones CRUD
 from app.controlador.EncounterCrud import (
@@ -43,6 +46,36 @@ async def handle_resource(request: Request, writer_function, resource_name: str)
 
 
 # Endpoints individuales
+@app.get("/patient/{patient_id}", response_model=dict)
+async def get_patient_by_id(patient_id: str):
+    status,patient = GetPatientById(patient_id)
+    if status=='success':
+        return patient  # Return patient
+    elif status=='notFound':
+        raise HTTPException(status_code=404, detail="Patient not found")
+    else:
+        raise HTTPException(status_code=500, detail=f"Internal error. {status}")
+
+@app.get("/patient", response_model=dict)
+async def get_patient_by_identifier(system: str, value: str):
+    print("Solicitud datos:",system,value)
+    status,patient = GetPatientByIdentifier(system,value)
+    if status=='success':
+        return patient  # Return patient
+    elif status=='notFound':
+        raise HTTPException(status_code=204, detail="Patient not found")
+    else:
+        raise HTTPException(status_code=500, detail=f"Internal error. {status}")
+
+
+@app.post("/patient", response_model=dict)
+async def add_patient(request: Request):
+    new_patient_dict = dict(await request.json())
+    status,patient_id = WritePatient(new_patient_dict)
+    if status=='success':
+        return {"_id":patient_id}  # Return patient id
+    else:
+        raise HTTPException(status_code=500, detail=f"Validating error: {status}")
     
 @app.post("/encounters")
 async def post_encounter(request: Request):
